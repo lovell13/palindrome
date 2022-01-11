95811f883add527af1e645bd68c9004c3971f0e4
Troy <alcinov@gmail.com>
2022-01-10 23:57:28 -0400

    Conduct some tests on testing suite

--- test/test.js
+++ test/test.js
@@ -30,6 +30,7 @@ describe("Phrase", function() {
         it("should return only letters", function() {
             let punctuatedPalindrome = new Phrase("Madam, I'm Adam.");
             assert.strictEqual(punctuatedPalindrome.letters(), "MadamImAdam");
+            
         });
     });
 });

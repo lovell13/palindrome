9f442286a606620fe00696ee604281503215cc67
Troy <alcinov@gmail.com>
2022-01-11 00:06:36 -0400

    Green all tests, first draft

--- index.js
+++ index.js
@@ -11,12 +11,13 @@ function Phrase(content) {
 
     // Returns content processed for palindrome testing.
     this.processedContent = function processedContent() {
-        return this.content.toLowerCase();
+        return this.letters().toLowerCase();
     }
         
     // Returns the letters in the content.
     this.letters = function letters() {
-        return this.content; // stub return value
+        //return this.content.match(/[a-zA-Z]/g).join(""); 
+        return this.content.match(/[a-zA-Z]/g).join(""); 
     }
     
     // Returns true if the phrase is a palindrome, false otherwise.
